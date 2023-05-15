6071d9ef95f440f1f1fff8fa7b30346bc3d96dcc
Rajat Jain <rajat.apple007@gmail.com>
2023-05-15 10:49:07 -0700

    Authentication and verification

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,9 +1,10 @@
 import express from "express"
 import { createHotel, deleteHotel, readAllHotels, readHotel, updateHotel } from "../controllers/hotel.js"
+import {verifyAdmin} from '../utils/verifyToken.js'
 const router = express.Router()
 
 // CREATE
-router.post("/",createHotel)
+router.post("/",verifyAdmin,createHotel)
 
 // GET
 router.get("/:id",readHotel)
@@ -12,9 +13,9 @@ router.get("/:id",readHotel)
 router.get("/",readAllHotels)
 
 // UPDATE
-router.put("/:id",updateHotel)
+router.put("/:id",verifyAdmin,updateHotel)
 
 // DELETE
-router.delete("/:id",deleteHotel)
+router.delete("/:id",verifyAdmin,deleteHotel)
 
 export default router
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,18 +1,33 @@
 import express from "express"
 const router = express.Router()
 import { readAllUsers,readUser,updateUser,deleteUser } from "../controllers/user.js"
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js"
 
+// // CHECK AUTH
+// router.get("/checkAuth",verifyToken,(req,res,next)=>{
+//     res.send("Authentication successful.")
+// })
+
+// // CHECK USER
+// router.get("/checkUser/:id",verifyUser,(req,res,next)=>{
+//     res.send("Logged in and okay to delete.")
+// })
+
+// CHECK ADMIN
+router.get("/checkAdmin/:id",verifyAdmin,(req,res,next)=>{
+    res.send("You're an admin.")
+})
 
 // GET
-router.get("/:id",readUser)
+router.get("/:id",verifyUser,readUser)
 
 // GET ALL
-router.get("/",readAllUsers)
+router.get("/",verifyAdmin,readAllUsers)
 
 // UPDATE
-router.put("/:id",updateUser)
+router.put("/:id",verifyUser,updateUser)
 
 // DELETE
-router.delete("/:id",deleteUser)
+router.delete("/:id",verifyUser,deleteUser)
 
 export default router
